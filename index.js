7812e785ec3a44de16ef9b8b37ee9987d7694473
Alex <lakotalegendary>
2019-07-25 09:34:21 +0400

    Add jquery

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 let result = fetch('https://frontend-test-api.alex93.now.sh/api/languages').then(res=>res.json());
 console.log(result);
+
+$.ajax({
+    url: 'https://frontend-test-api.alex93.now.sh/api/languages',
+    method: 'GET',
+});
+
+$(document).ready(()=>{
+
+});
