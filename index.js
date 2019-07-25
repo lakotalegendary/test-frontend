160b4261650dffab4035ef0a53e131d575e8df87
Alex <lakotalegendary>
2019-07-25 13:40:42 +0400

    Fetching data in index.js and pushes into index.html, rename styles

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-let result = fetch('https://frontend-test-api.alex93.now.sh/api/languages').then(res=>res.json());
-console.log(result);
 
-$.ajax({
-    url: 'https://frontend-test-api.alex93.now.sh/api/languages',
-    method: 'GET',
-});
+// $.getJSON('https://frontend-test-api.alex93.now.sh/api/languages?group=backend', function(data){
+//     console.log(data.data);
+//
+//     var items = [];
+//
+//     $.each(data.data, function(index, val){
+//         items.push('<li>' +
+//                         '<div id="item-'+ index +'">' +
+//                             '<p>' + val.name + ' ' + (val.year ? val.year : '') + ' ' +  (val.docs ? '<a href="' + val.docs + '">Docs</a>' : '' ) + '</p>' +
+//                             (val.logo ? '<img src="' + val.logo + '" alt="'+ val.name +'"/>' : '' ) +
+//                         '</div>' +
+//                     '</li>');
+//     });
+//
+//     $('<ul/>', {
+//         'class': 'list-data',
+//         html: items.join('')
+//     }).appendTo('#result');
+// });
 
-$(document).ready(()=>{
 
+$(document).ready(() => {
+    $('#btn').click(function(e) {
+        let url = 'https://frontend-test-api.alex93.now.sh/api/languages?group=' + $('#select :selected').val();
+        $.getJSON(url, function(data){
+            console.log(data.data);
+
+            var items = [];
+
+            $.each(data.data, function(index, val){
+                val.logo && items.push('<li>' +
+                                '<div id="item-'+ index +'">' +
+                                    (val.logo ? '<img src="' + val.logo + '" alt="'+ val.name +'"/>' : '' ) +
+                                    '<div><p>' + val.name + '</p><p>' + (val.year ? 'Основан в ' + val.year : '') + '</p><p>' +  (val.docs ? '<a href="' + val.docs + '">Документация</a>' : '' ) + '</p></div>' +
+                                '</div>' +
+                            '</li>');
+            });
+
+            let $list = $('<ul/>', {
+                'class': 'list-data',
+                html: items.join('')
+            });
+            $('#result').html($list);
+        });
+    });
 });
