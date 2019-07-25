a31455c5e616fb96a65eb4e47fd14d17f71bf03c
Alex <lakotalegendary>
2019-07-25 13:55:58 +0400

    Adding comments to index.js, fixes styles

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
-
-// $.getJSON('https://frontend-test-api.alex93.now.sh/api/languages?group=backend', function(data){
-//     console.log(data.data);
-//
-//     var items = [];
-//
-//     $.each(data.data, function(index, val){
-//         items.push('<li>' +
-//                         '<div id="item-'+ index +'">' +
-//                             '<p>' + val.name + ' ' + (val.year ? val.year : '') + ' ' +  (val.docs ? '<a href="' + val.docs + '">Docs</a>' : '' ) + '</p>' +
-//                             (val.logo ? '<img src="' + val.logo + '" alt="'+ val.name +'"/>' : '' ) +
-//                         '</div>' +
-//                     '</li>');
-//     });
-//
-//     $('<ul/>', {
-//         'class': 'list-data',
-//         html: items.join('')
-//     }).appendTo('#result');
-// });
-
-
+//on document ready use click handler on button
 $(document).ready(() => {
     $('#btn').click(function(e) {
-        let url = 'https://frontend-test-api.alex93.now.sh/api/languages?group=' + $('#select :selected').val();
-        $.getJSON(url, function(data){
-            console.log(data.data);
-
+        //getJSON to fetch data from url
+        //if we press the button on initial position (Выберите направление) must we get all items from server???
+        $.getJSON('https://frontend-test-api.alex93.now.sh/api/languages', {group: $('#select :selected').val()},function(data){
+            //console.log(data.data);
             var items = [];
-
+            //create array with html elements with conditions
             $.each(data.data, function(index, val){
+                // if element haven't logo shouldn't display it
                 val.logo && items.push('<li>' +
-                                '<div id="item-'+ index +'">' +
-                                    (val.logo ? '<img src="' + val.logo + '" alt="'+ val.name +'"/>' : '' ) +
-                                    '<div><p>' + val.name + '</p><p>' + (val.year ? 'Основан в ' + val.year : '') + '</p><p>' +  (val.docs ? '<a href="' + val.docs + '">Документация</a>' : '' ) + '</p></div>' +
-                                '</div>' +
-                            '</li>');
+                                            '<div id="item-'+ index +'">' +
+                                                (val.logo ? '<img src="' + val.logo + '" alt="'+ val.name +'"/>' : '' ) +
+                                                '<div><p>' + val.name + '</p><p>' + (val.year ? 'Основан в ' + val.year : '') + '</p><p>' +  (val.docs ? '<a href="' + val.docs + '">Документация</a>' : '' ) + '</p></div>' +
+                                            '</div>' +
+                                        '</li>');
             });
-
+            //create list of elements we need
             let $list = $('<ul/>', {
                 'class': 'list-data',
                 html: items.join('')
             });
+            //.html to display our elements on page
             $('#result').html($list);
         });
     });
